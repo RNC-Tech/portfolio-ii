a0c782c9c5c5e9b19d0e63a10ee2facdad155a28
RNC-Tech <rncsimbaya@gmail.com>
2025-06-29 22:40:08 +0800

    Update Next.js configuration to set basePath and image path based on production environment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? '/portfolio-ii/projects' : '',
   output: 'export',
   distDir: 'dist',
   images: {
     unoptimized: true,
+    path: isProd ? '/portfolio-ii/projects/_next/image' : '/_next/image',
   }
 };
 
