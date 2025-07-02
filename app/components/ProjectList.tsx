cb25e728b76e8681af6fbc9c9890469148c6907d
RNC-Tech <rncsimbaya@gmail.com>
2025-07-02 16:55:15 +0800

    Update ProjectList component styles for improved visual consistency, changing card background color to gray and enhancing button appearance with rounded edges and active scaling effects.

--- app/components/ProjectList.tsx
+++ app/components/ProjectList.tsx
@@ -7,11 +7,11 @@ const ProjectList = () => {
   return (
     <div className="flex flex-wrap justify-center gap-6">
       {projects.map((project) => (
-        <div key={project.id} className="card bg-base-100 w-full md:w-96 shadow-2xl transition-transform duration-300 hover:scale-105 hover:shadow-lg rounded-2xl">
+        <div key={project.id} className="card w-full md:w-96 shadow-2xl bg-gray-800 transition-transform duration-300 hover:scale-105 hover:shadow-lg rounded-2xl">
             <Link href={`/projects/projectdetails/${project.id}`}>
                 <div>
                     <Image 
-                      className='max-h-50 object-fill rounded-t-2xl' 
+                      className='sm:w-full max-h-50 object-cover rounded-t-2xl' 
                       src={project.image} 
                       alt={project.title}
                       width={384}
@@ -25,7 +25,7 @@ const ProjectList = () => {
             <p className='text-gray-400 mb-12'>{project.description.length > 100 ? `${project.description.substring(0, 100)}...` : project.description}</p>
             <div className="card-actions justify-end">
               <Link href={`/projects/projectdetails/${project.id}`}>
-                <button className="btn btn-primary">View Project</button>
+                <button className="btn btn-primary rounded-full active:scale-95">View Project</button>
               </Link>
             </div>
           </div>
