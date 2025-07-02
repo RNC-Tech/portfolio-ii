0400059dcb80895fa9b68b34155c334d4a228ea0
RNC-Tech <rncsimbaya@gmail.com>
2025-07-02 16:55:00 +0800

    Refactor layout styles to improve responsiveness by adjusting padding for mobile and medium screens.

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <NavBar />
         <Transition>
-          <div className="px-42 py-16">
+          <div className="px-4 md:px-42 py-8 md:py-16">
             {children}
           </div>
         </Transition>
