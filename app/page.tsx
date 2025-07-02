3ba8a49fe0990711df27e0a69a101eba7765015c
RNC-Tech <rncsimbaya@gmail.com>
2025-07-02 17:50:39 +0800

    Enhance image styling by adding bounce animation for improved visual engagement in page layout.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
     <div className='flex justify-center items-start min-h-screen px-2 sm:px-4'>
       <div className='card lg:min-w-250 bg-gradient-to-tl from-gray-900 to-gray-800 shadow-xl flex flex-col md:flex-row items-center md:items-start justify-start max-w-4xl h-fit px-4 sm:px-8 md:px-16 lg:px-24 py-8 sm:py-16 md:py-24 rounded-2xl'>
         <div className='w-full md:w-1/2 flex justify-center mb-8 md:mb-0 md:mr-8'>
-          <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDBvMjN0ZHpjd2V5NmhoYTM2aWphdXNnZXh0ZGI4MmQ4cDlsZTc4ZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/935gCxva4XOcgtBbdO/giphy.gif" alt="Visuals with Purpose" className='w-64 h-64 sm:w-80 sm:h-80 object-contain rounded-lg ' />
+          <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDBvMjN0ZHpjd2V5NmhoYTM2aWphdXNnZXh0ZGI4MmQ4cDlsZTc4ZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/935gCxva4XOcgtBbdO/giphy.gif" alt="Visuals with Purpose" className='w-64 h-64 sm:w-80 sm:h-80 object-contain rounded-lg animate-bounce ' />
         </div>
         <div className='w-full md:w-fit flex justify-center'>
           <article className='prose prose-invert max-w-none'>
