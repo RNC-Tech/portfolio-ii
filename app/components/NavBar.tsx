be18461e80adacc07772ed33939dcaa1e52768ed
RNC-Tech <rncsimbaya@gmail.com>
2025-07-05 05:36:29 +0800

    Add sound effects for button interactions in NavBar component, enhancing user experience with hover and click audio feedback.

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,21 @@
 'use client'
 import Link from 'next/link'
 import React from 'react'
+import useSound from './useSound'
+
+const buttonHoverSound = 'https://d1aht2gieyw871.cloudfront.net/z8wx6w%2Ffile%2F443278c0d3195c51cb44af6a3d99fa4b_e8ac2773334397a4e048e9b174f041de.wav?response-content-disposition=inline%3Bfilename%3D%22443278c0d3195c51cb44af6a3d99fa4b_e8ac2773334397a4e048e9b174f041de.wav%22%3B&response-content-type=audio%2Fx-wav&Expires=1751683615&Signature=DwOEtbT8qs59JPSYTXktJonmAIrZR481vDRMYErm~ah1X6wyT1e-33nkyY9Bn6hB~xtmY8Eagex0tQYcYb2o8SW22gU3aOG0gb-3OUlRdNZTID2euxZ7cyx6c6xFlrrbQBGFXXUErRUt~7dpyEj5GFO28olszWc8g52j44U3md7Ns6PMgnF8~PCRzCzgpZTlA0Kf85J7Hg-9JhSezunMsJRbNgsi1V9cAuOID1xYgtosgJB8SNvscRLK4SGSx4s1e3lsSh2zwEcaLwjIMV79m8eG4Uyk9sixidPLgQhweVBmXe6K8pDQaF0UAspjmYReHa8Kr6cItUXu0oWCVKfqlQ__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ';
+const buttonClickSound = 'https://dz0i85a16ad4a.cloudfront.net/t8ux6w%2Ffile%2F9f99ae93281d3e862878318fd3696264_96dac53aebf63ab3236b8c1c1147ef19.wav?response-content-disposition=inline%3Bfilename%3D%229f99ae93281d3e862878318fd3696264_96dac53aebf63ab3236b8c1c1147ef19.wav%22%3B&response-content-type=audio%2Fx-wav&Expires=1751683645&Signature=E4nBeO5piFCL7Bmnd602FqJq5FDo9YfcgI6nn2CDV1OO-6IKoamDdQBnMeS1xRdUzEwGkkIKk~VYlAlYrroUIHHlr1Q7o0~0jgyBS9dZ6dzvedpSTnsdu8hC89ifPY8aBd-SdKg7pZLD4f8PYK-QaWBq4RToTuIXhh6-LAiBwskp80a4cMpYBFi~nN6v4n0Oq8GucZSouNRtkONxYvdsbGPoHmglKy1cvA26ULUVznxui8Ltgg11ACWBnCFV8O~dEKcYDVUCp85htyReXiUtol6yvCaLRJviNr7gDefwtddNcf67OJTYMxpNTiOE6Oii2Rx-M1i5-M7hKIN8wHMa0Q__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ';
 
 const NavBar = () => {
+  const playButtonHover = useSound(buttonHoverSound, 0.2)
+  const playButtonClick = useSound(buttonClickSound, 0.2)
+
   return (
     <div className='sticky top-0 z-50 w-full flex justify-center items-center h-24 p-4'>
         <div className='rounded-full flex justify-center items-center h-full w-fit px-4 bg-gray-800 2xl:px-16 2xl:px-16 gap-4'>
             <div className="relative flex flex-col items-center group">
                 <Link href='/'>
-                    <button className='btn btn-primary btn-ghost btn-circle hover:scale-180 transition-all duration-100 active:scale-150'>
+                    <button className='btn btn-primary btn-ghost btn-circle hover:scale-180 transition-all duration-100 active:scale-150' onMouseEnter={playButtonHover} onClick={playButtonClick}>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
@@ -30,7 +37,7 @@ const NavBar = () => {
             
             <div className="relative flex flex-col items-center group">
                 <Link href='/about'>
-                    <button className='btn btn-primary btn-ghost btn-circle hover:scale-180 transition-all duration-100 active:scale-150'>
+                    <button className='btn btn-primary btn-ghost btn-circle hover:scale-180 transition-all duration-100 active:scale-150' onMouseEnter={playButtonHover} onClick={playButtonClick}>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
@@ -52,7 +59,7 @@ const NavBar = () => {
             
             <div className="relative flex flex-col items-center group">
                 <Link href='/projects'>
-                    <button className='btn btn-primary btn-ghost btn-circle hover:scale-180 transition-all duration-100 active:scale-150'>
+                    <button className='btn btn-primary btn-ghost btn-circle hover:scale-180 transition-all duration-100 active:scale-150' onMouseEnter={playButtonHover} onClick={playButtonClick}>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
