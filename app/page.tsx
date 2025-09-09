076030d8fea8e942b8dc23e51c0165838b3f8939
RNC-Tech <rncsimbaya@gmail.com>
2025-09-10 00:21:23 +0800

    refactor(image-loader): update image source path for improved organization

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ const Page = () => {
           <div className="flex-shrink-0 flex justify-center lg:justify-start w-full lg:w-auto">
             <div className="w-64 h-64 sm:w-80 sm:h-80 lg:w-96 lg:h-96 rounded-2xl">
               <Image
-                src="/hero-animation.gif"
+                src="/portfolio-ii/hero-animation.gif"
                 alt="Visuals with Purpose"
                 width={384}
                 height={384}
